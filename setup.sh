be360ae90368c3f713d74c4a2197cf886603e4b5
Deedax Inc <34832399+Daheer@users.noreply.github.com>
2023-03-16 12:28:15 +0100

    Delete setup.sh

--- setup.sh
+++
@@ -1,5 +0,0 @@
-pip install -r requirements.txt
-pip install git+https://github.com/openai/whisper.git
-sudo apt update && sudo apt install ffmpeg 
-sudo apt install streamlit
-streamlit run app.py --server.port 80
